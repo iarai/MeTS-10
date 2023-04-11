303d846cb8abe1ba6e0f89e24a0d84c146ff2e50
Christian Eichenberger <christian.eichenberger@iarai.ac.at>
2023-04-11 08:39:18 +0200

    bugfix lon,lat unpacking

--- analysis/val03_meta/bearing.py
+++ analysis/val03_meta/bearing.py
@@ -9,23 +9,22 @@ MARGIN = 10
 
 
 def main(DATA_ROOT: Path, RELEASE: str, YEAR: str, CITY: str):
-
     print(f"{DATA_ROOT}/{RELEASE}/{YEAR}/{CITY}")
 
     gdf_edges = gpd.read_parquet(DATA_ROOT / RELEASE / YEAR / "road_graph" / CITY / "road_graph_edges.parquet")
     num_diagonals = 0
     num_horizontal_verticals = 0
-    ignored = 0
+
+    diagonal = []
+    horizontal_vertical = []
 
     for g in gdf_edges["geometry"]:
         g: LineString = g
         coordinates = list(g.coords)
         orientations = set()
-        for (lat_from, lon_from), (lat_to, lon_to) in zip(coordinates, coordinates[1:]):
+        for (lon_from, lat_from), (lon_to, lat_to) in zip(coordinates, coordinates[1:]):
             alpha = get_bearing(lat_from, lon_from, lat_to, lon_to)
-            if not (0 <= alpha < 360):
-                ignored += 1
-                continue
+            assert 0 <= alpha < 360, (alpha, coordinates)
             # clock-wise
             for b, h in zip([0, 90, 180, 270, 45, 90 + 45, 180 + 45, 270 + 45], ["N", "E", "S", "W", "NE", "SE", "SW", "NW"]):
                 if (b - MARGIN) % 360 <= alpha < (b + MARGIN) % 360:
@@ -40,14 +39,22 @@ def main(DATA_ROOT: Path, RELEASE: str, YEAR: str, CITY: str):
                 "W",
             ]:
                 num_horizontal_verticals += 1
+                diagonal.append(False)
+                horizontal_vertical.append(True)
             else:
                 num_diagonals += 1
+                diagonal.append(True)
+                horizontal_vertical.append(False)
         else:
-            pass
-    print(f"num_diagonals={num_diagonals} ({num_diagonals / len(gdf_edges)*100:.2f}%)")
-    print(f"num_horizontal_verticals={num_horizontal_verticals} ({num_horizontal_verticals / len(gdf_edges)*100:.2f}%)")
-    # TODO what happens here?
-    print(f"ignored={ignored}, what happens here?")
+            diagonal.append(False)
+            horizontal_vertical.append(False)
+    print(f"num_diagonals={num_diagonals} ({num_diagonals / len(gdf_edges) * 100:.2f}%)")
+    print(f"num_horizontal_verticals={num_horizontal_verticals} ({num_horizontal_verticals / len(gdf_edges) * 100:.2f}%)")
+    gdf_edges["diagonal"] = diagonal
+    gdf_edges["horizontal_vertical"] = horizontal_vertical
+
+    # TODO merge with data (1): coverage
+    # TODO merge with data (2): mapped volume
 
 
 if __name__ == "__main__":
@@ -72,48 +79,33 @@ if __name__ == "__main__":
         for CITY in CITIES:
             main(DATA_ROOT=DATA_ROOT, RELEASE=RELEASE, YEAR=YEAR, CITY=CITY)
 
-
-# /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/antwerp
-# num_diagonals=20612 0.25239080656813645
-# num_horizontal_verticals=15745 0.19279513144844307
-# ignored=0, what happens here?
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/bangkok
-# num_diagonals=0 0.0
-# num_horizontal_verticals=0 0.0
-# ignored=1689699, what happens here?
+# num_diagonals=91609 (13.18%)
+# num_horizontal_verticals=201316 (28.97%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/barcelona
-# num_diagonals=33847 0.2848762340821291
-# num_horizontal_verticals=20562 0.17306187033405435
-# ignored=0, what happens here?
+# num_diagonals=34724 (29.23%)
+# num_horizontal_verticals=22003 (18.52%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/berlin
-# num_diagonals=19894 0.223824846425598
-# num_horizontal_verticals=19164 0.21561170990751782
-# ignored=0, what happens here?
+# num_diagonals=19940 (22.43%)
+# num_horizontal_verticals=20875 (23.49%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/chicago
-# num_diagonals=39183 0.20889801140907394
-# num_horizontal_verticals=62914 0.3354161113184411
-# ignored=0, what happens here?
+# num_diagonals=9399 (5.01%)
+# num_horizontal_verticals=118970 (63.43%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/istanbul
-# num_diagonals=61639 0.22820046721878945
-# num_horizontal_verticals=57874 0.21426164992651114
-# ignored=0, what happens here?
+# num_diagonals=61564 (22.79%)
+# num_horizontal_verticals=61255 (22.68%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/melbourne
-# num_diagonals=0 0.0
-# num_horizontal_verticals=0 0.0
-# ignored=774842, what happens here?
+# num_diagonals=31915 (13.84%)
+# num_horizontal_verticals=103833 (45.02%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2021/moscow
-# num_diagonals=9410 0.19654531403387848
-# num_horizontal_verticals=9845 0.20563109635106627
-# ignored=0, what happens here?
+# num_diagonals=13259 (27.69%)
+# num_horizontal_verticals=10177 (21.26%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2022/london
-# num_diagonals=64074 0.2363700083002859
-# num_horizontal_verticals=55953 0.2064115097297796
-# ignored=0, what happens here?
+# num_diagonals=65380 (24.12%)
+# num_horizontal_verticals=59654 (22.01%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2022/madrid
-# num_diagonals=36153 0.25210945454038297
-# num_horizontal_verticals=30255 0.21098032105549436
-# ignored=0, what happens here?
+# num_diagonals=35551 (24.79%)
+# num_horizontal_verticals=32018 (22.33%)
 # /iarai/public/t4c/data_pipeline/release20221026_residential_unclassified/2022/melbourne
-# num_diagonals=0 0.0
-# num_horizontal_verticals=0 0.0
-# ignored=774842, what happens here?
+# num_diagonals=31915 (13.84%)
+# num_horizontal_verticals=103833 (45.02%)
